565c24ab92d5a66fcc65810e90cb3a39ac254a81
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-08-02 13:40:04 -0600

    Add mutation rate parameter

--- evolution/genetic_knapsack.py
+++ evolution/genetic_knapsack.py
@@ -3,9 +3,9 @@ from numpy import random
 from knapsack_items import Item, item_list
 
 
-def generate_initital_population(population_size: int, individual_size: int):
+def generate_initital_population(population_size: int, individual_size: int, mutation_rate: float):
     # Population could be randomly initialized individuals instead
-    population = [mutate_individual([0] * individual_size)] * population_size
+    population = [mutate_individual([0] * individual_size, mutation_rate)] * population_size
     return population
 
 
@@ -49,9 +49,11 @@ def uniform_crossover(parent_a: list[int], parent_b: list[int]):
     return children
 
 
-def mutate_individual(individual: list[int]):
-    mutation_index = rng.integers(len(individual))
-    individual[mutation_index] = 0 if individual[mutation_index] else 1
+def mutate_individual(individual: list[int], mutation_rate: float):
+    mutations = round(len(individual) * mutation_rate)
+    mutation_indices = rng.integers(len(individual), size=mutations)
+    for mutation_index in mutation_indices:
+        individual[mutation_index] = 0 if individual[mutation_index] else 1
 
     return individual
 
@@ -59,6 +61,7 @@ def mutate_individual(individual: list[int]):
 def generate_new_population(
     current_population: list[list[int]],
     survival_rate: float,
+    mutation_rate: float,
     knapsack_items: list[Item],
     knapsack_capacity: int,
 ):
@@ -77,7 +80,7 @@ def generate_new_population(
     for a, b in zip(group_a, group_b):
         children.extend(uniform_crossover(a, b))
 
-    children = [mutate_individual(child) for child in children]
+    children = [mutate_individual(child, mutation_rate) for child in children]
 
     new_population = current_population[number_of_children:] + children
     return new_population
@@ -86,18 +89,19 @@ def generate_new_population(
 def run_ga(
     population_size: int,
     survival_rate: float,
+    mutation_rate: float,
     number_of_generations: int,
     knapsack_items: list,
     knapsack_capacity: int,
 ):
-    population = generate_initital_population(population_size, len(knapsack_items))
+    population = generate_initital_population(population_size, len(knapsack_items), mutation_rate)
     global_best_fitness = max(
         [calculate_individual_fitness(i, knapsack_items, knapsack_capacity) for i in population]
     )
     print(f"{global_best_fitness = }")
     for i in range(number_of_generations):
         population = generate_new_population(
-            population, survival_rate, knapsack_items, knapsack_capacity
+            population, survival_rate, mutation_rate, knapsack_items, knapsack_capacity
         )
 
         best_fitness = max(
@@ -117,6 +121,7 @@ def run_ga(
 if __name__ == "__main__":
     population_size = 16
     survival_rate = 0.15
+    mutation_rate = 0.05
     number_of_generations = 10000
     knapsack_items = item_list
     knapsack_capacity = 6_404_180
@@ -124,6 +129,11 @@ if __name__ == "__main__":
     rng = random.default_rng()
 
     result = run_ga(
-        population_size, survival_rate, number_of_generations, knapsack_items, knapsack_capacity
+        population_size,
+        survival_rate,
+        mutation_rate,
+        number_of_generations,
+        knapsack_items,
+        knapsack_capacity,
     )
     print(result)
