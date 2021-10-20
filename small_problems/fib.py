121098cb02d4a4ec9e321b35fe6f11639cd66748
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-10-20 09:40:07 -0600

    Re-add recursive fib solution to compare runtime

--- small_problems/fib.py
+++ small_problems/fib.py
@@ -1,8 +1,9 @@
 import cProfile
 import pstats
+from functools import cache
 
 
-def fib(n: int):
+def fib_gen(n: int):
     yield 0
     if n > 0:
         yield 1
@@ -13,10 +14,27 @@ def fib(n: int):
         yield next
 
 
+@cache
+def fib_recursive(n: int) -> int:
+    if n < 2:
+        return n
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+
 if __name__ == "__main__":
+    n = 500_000
     with cProfile.Profile() as p:
-        for i in fib(10):
-            print(i)
+        for i in fib_gen(n):
+            ...
+            # print(i)
 
     stats = pstats.Stats(p)
-    stats.sort_stats(pstats.SortKey.CALLS).print_stats()
+    stats.sort_stats(pstats.SortKey.TIME).print_stats()
+
+    with cProfile.Profile() as p:
+        for i in range(n + 1):
+            fib_recursive(i)
+            # print(fib_recursive(i))
+
+    stats = pstats.Stats(p)
+    stats.sort_stats(pstats.SortKey.TIME).print_stats()
